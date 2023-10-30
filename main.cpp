9c8f8dc6b6cc6058b0382de61d2b80e3404dafa5
Daniel <dabsi11@hotmail.com>
2023-10-30 16:05:58 -0400

    Update main.cpp to version 1.1
    Version 1.1 With save solution and speed optimisation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <filesystem>
 #include <chrono>
+#include <ctime>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -25,14 +26,55 @@ void displayFileContent(const string &path) {
     }
 }
 
-void displayFilesAndContent(const string &path) {
-    for (const auto &entry : fs::directory_iterator(path)) {
+void processDirectoryForSaving(const string &directoryPath, ofstream &outputFile);
+
+void displayFileContentToStream(const string &path, ofstream &outputFile) {
+    ifstream file(path);
+    if (file.is_open()) {
+        string line;
+        outputFile << path << ":\n";
+        while (getline(file, line)) {
+            outputFile << line << '\n';
+        }
+        outputFile << "----------------------------------------\n";
+        file.close();
+    } else {
+        outputFile << "Unable to open file: " << path << '\n';
+    }
+}
+
+void saveContentToFile(const string &directoryPath) {
+    auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    tm current_time = *localtime(&now);
+
+    string filename = "prompt_" + to_string(current_time.tm_year + 1900) + "-" +
+                      to_string(current_time.tm_mon + 1) + "-" +
+                      to_string(current_time.tm_mday) + "_" +
+                      to_string(current_time.tm_hour) + ":" +
+                      to_string(current_time.tm_min) + ":" +
+                      to_string(current_time.tm_sec) + ".txt";
+
+    string filePath = directoryPath + "/" + filename;
+
+    ofstream outputFile(filePath);
+    if (outputFile.is_open()) {
+        outputFile << "Directory: " << directoryPath << "\n";
+        processDirectoryForSaving(directoryPath, outputFile);
+        outputFile.close();
+        cout << "The file " << filename << " has been saved in the directory: " << directoryPath << endl;
+    } else {
+        cout << "Unable to create/open the file for writing." << endl;
+    }
+}
+
+void processDirectoryForSaving(const string &directoryPath, ofstream &outputFile) {
+    for (const auto &entry : fs::directory_iterator(directoryPath)) {
         string filename = entry.path().filename();
         if (entry.is_regular_file() && !shouldIgnoreFile(filename)) {
-            displayFileContent(entry.path());
+            displayFileContentToStream(entry.path(), outputFile);
         } else if (entry.is_directory() && filename != "node_modules" && filename != "UNIT") {
-            cout << "Directory: " << entry.path() << "\n";
-            displayFilesAndContent(entry.path()); // Recursively explore directories
+            outputFile << "Directory: " << entry.path() << "\n";
+            processDirectoryForSaving(entry.path(), outputFile); // Recursively explore directories
         }
     }
 }
@@ -43,38 +85,7 @@ int main() {
     getline(cin, directoryPath);
 
     if (fs::is_directory(directoryPath)) {
-        displayFilesAndContent(directoryPath);
-
-        char saveToFile;
-        cout << "Do you want to save the content to a file? (y/n): ";
-        cin >> saveToFile;
-
-        if (saveToFile == 'y' || saveToFile == 'Y') {
-            auto now = chrono::system_clock::now();
-            time_t now_c = chrono::system_clock::to_time_t(now);
-            tm current_time = *localtime(&now_c);
-
-            string filename = "promp_" + to_string(current_time.tm_year + 1900) + "-" +
-                              to_string(current_time.tm_mon + 1) + "-" +
-                              to_string(current_time.tm_mday) + "/" +
-                              to_string(current_time.tm_hour) + ":" +
-                              to_string(current_time.tm_min) + ":" +
-                              to_string(current_time.tm_sec) + ".txt";
-
-            string filePath = directoryPath + "/" + filename;
-
-            ofstream outputFile(filePath);
-            if (outputFile.is_open()) {
-                outputFile << "Content from directory: " << directoryPath << "\n";
-                outputFile << "Additional information, if any...\n";
-                outputFile.close();
-                cout << "The file " << filename << " has been saved in the directory: " << directoryPath << endl;
-            } else {
-                cout << "Unable to create/open the file for writing." << endl;
-            }
-        } else {
-            cout << "Content will not be saved to a file." << endl;
-        }
+        saveContentToFile(directoryPath);
     } else {
         cout << "The given path is not a directory.\n";
     }
